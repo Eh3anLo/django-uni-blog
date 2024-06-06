649a99b86f927f1711cedc37db162cf505d74502
ehsan <ehsunlotfy@gmail.com>
2024-06-06 16:49:06 +0330

    Implement Signup page , handle signup errors message

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,15 @@ from  django.contrib.auth.forms import UserChangeForm , UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    # def __init__(self, *args, **kwargs):
+        # super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        # for fieldname in ['username', 'password1', 'password2']:
+        #     self.fields[fieldname].help_text = None
+            
     class Meta:
         model = CustomUser
-        fields = ('username' , 'email' , 'age' ,)
+        fields = ('username' ,'first_name','last_name' , 'email',)
 
 
 class CustomUserChangeForm(UserChangeForm):

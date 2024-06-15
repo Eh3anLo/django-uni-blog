ef38f4787edb1d9af57349ee7cf61b2c054bb1ea
ehsan <ehsunlotfy@gmail.com>
2024-06-16 00:47:37 +0330

    make email uniqe for authentication

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(blank=True , null=True)
+    email = models.EmailField(unique=True)
+

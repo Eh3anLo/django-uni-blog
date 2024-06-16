1840ac76196246e1c582486be3e871904746d053
ehsan <ehsunlotfy@gmail.com>
2024-06-16 15:07:02 +0330

    Show authors article list in user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -6,6 +7,9 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     img = models.ImageField(upload_to='media/uploads/images/2024/06' , blank=True)
 
+    def get_absolute_url(self):
+        return reverse("user_profile", kwargs={"username": self.username})
+    
     # def get_absolute_url(self):
     #     return reverse("model_detail", kwargs={"pk": self.pk})
     
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render , get_object_or_404
 from .models import UserProfile
 from accounts.models import CustomUser
+from articles.models import Article
 # Create your views here.
 def user_profile_view(request , username):
     user_id = get_object_or_404(CustomUser , username = username)
     user_profile = get_object_or_404(UserProfile , user = user_id)
-    return render(request , 'profiles/profile.html' , {"user_profile" : user_profile})
+    if user_profile:
+        user_articles = Article.objects.filter(author = user_id)
+    return render(request , 'profiles/profile.html' , {"user_profile" : user_profile , "articles" : user_articles})
